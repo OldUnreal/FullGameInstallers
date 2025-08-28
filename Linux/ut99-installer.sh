eb6a6291a60af939d548a3908708a16f54f51189
Christian Weiske <cweiske@cweiske.de>
2025-08-28 18:54:13 +0200

    Change inline variable quoting mode
    .. which is more readable in my eyes, especially when concatenating
    multiple variables

--- Linux/ut99-installer.sh
+++ Linux/ut99-installer.sh
@@ -8,7 +8,7 @@ setVariables() {
 	game_name="Unreal Tournament"
 	game_folder='UnrealTournament'
 	iso_name='UT_GOTY_CD1.ISO'
-	iso_url="https://files.oldunreal.net/$iso_name"
+	iso_url="https://files.oldunreal.net/${iso_name}"
 	latest_release='https://api.github.com/repos/OldUnreal/UnrealTournamentPatches/releases/latest'
 	delete_folders="
 DirectX7
@@ -82,9 +82,9 @@ getUTFiles() {
 	mkdir $game_folder
 	cd $game_folder
 
-	echo "Downloading $game_name files..."
+	echo "Downloading ${game_name} files..."
 	wget -nv --show-progress "$iso_url"
-	echo -e "\xE2\x9C\x94 $game_name files downloaded"
+	echo -e "\xE2\x9C\x94 ${game_name} files downloaded"
 
 	echo "Extracting files..."
 	7z x $iso_name -y
@@ -98,14 +98,14 @@ deleteUnnecessaryFiles() {
 	do
 		if [ -d "$game_folder/$folder" ]
 		then
-			echo "Deleting folder $game_folder/$folder"
-			rm -rf "$game_folder/$folder"
+			echo "Deleting folder ${game_folder}/${folder}"
+			rm -rf "${game_folder}/${folder}"
 		fi
 	done
 	for extension in $delete_extensions
 	do
-		echo "Deleting $game_folder/System/*.$extension"
-		rm $game_folder/System/*.$extension
+		echo "Deleting ${game_folder}/System/*.${extension}"
+		rm ${game_folder}/System/*.${extension}
 	done
 	echo -e "\xE2\x9C\x94 Unnecessary files deleted"
 }
@@ -150,14 +150,14 @@ getArchitecture() {
 getPatch() {
 	getLatestRelease
 	getArchitecture
-	echo "Downloading patch $patch_ver"
+	echo "Downloading patch ${patch_ver}"
 	wget -P ./$game_folder -nv --show-progress $url_download
 	echo -e "\xE2\x9C\x94 Patch downloaded"
 
 	echo "Extracting and adding patch..."
-	patch_tar="./$game_folder/patch$patch_ver.tar.bz2"
-	mv ./$game_folder/*.tar.bz2 $patch_tar
-	tar -xf $patch_tar -C ./$game_folder/ --overwrite
+	patch_tar="./${game_folder}/patch${patch_ver}.tar.bz2"
+	mv ./${game_folder}/*.tar.bz2 $patch_tar
+	tar -xf $patch_tar -C ./${game_folder}/ --overwrite
 	rm ./patch_latest
 	echo -e "\xE2\x9C\x94 Patch added"
 }
@@ -165,22 +165,22 @@ getPatch() {
 decompressMaps() {
 	echo "Decompressing maps"
 	count=0
-	for file in `ls -1 $game_folder/Maps/*.unr.uz`
+	for file in `ls -1 ${game_folder}/Maps/*.unr.uz`
 	do
-		$game_folder/System$system_suffix/ucc-bin-$arc_suffix "decompress" "$curr_path/$file" "-nohomedir" > /dev/null 2>&1
+		${game_folder}/System${system_suffix}/ucc-bin-${arc_suffix} "decompress" "${curr_path}/${file}" "-nohomedir" > /dev/null 2>&1
 		basename=`echo $file | cut -d'/' -f3 | sed 's/\.uz//'`
-		if [ -e "$game_folder/System$system_suffix/$basename" ]
+		if [ -e "${game_folder}/System${system_suffix}/${basename}" ]
 		then
 			echo -n '.'
 			count=$((count + 1))
-			mv "$game_folder/System$system_suffix/$basename" "$game_folder/Maps/"
+			mv "${game_folder}/System${system_suffix}/${basename}" "${game_folder}/Maps/"
 			rm $file
 		else
-			echo "Failed to decompress $file"
+			echo "Failed to decompress ${file}"
 		fi
 	done
 	echo
-	echo -e "\xE2\x9C\x94 $count maps decompressed"
+	echo -e "\xE2\x9C\x94 ${count} maps decompressed"
 }
 
 addLinks() {
@@ -197,11 +197,11 @@ addLinks() {
 	if [[ $desktop_entry =~ ^[Yy]$ || $app_entry =~ ^[Yy]$ ]]; then
 		echo "Creating entry..."
 		echo "[Desktop Entry]" > $launcher_name
-		echo "Version=$patch_ver" >> $launcher_name
-		echo "Name=$game_name" >> $launcher_name
-		echo "Comment=$game_name" >> $launcher_name
-		echo "Exec=$curr_path/$game_folder/System$system_suffix/$game_executable$arc_suffix" >> $launcher_name
-		echo "Icon=$curr_path/$game_folder/System/Unreal.ico" >> $launcher_name
+		echo "Version=${patch_ver}" >> $launcher_name
+		echo "Name=${game_name}" >> $launcher_name
+		echo "Comment=${game_name}" >> $launcher_name
+		echo "Exec=${curr_path}/${game_folder}/System${system_suffix}/${game_executable}${arc_suffix}" >> $launcher_name
+		echo "Icon=${curr_path}/${game_folder}/System/Unreal.ico" >> $launcher_name
 		echo "Terminal=false" >> $launcher_name
 		echo "Type=Application" >> $launcher_name
 		echo "Categories=ApplicationCategory;" >> $launcher_name
@@ -237,9 +237,9 @@ deleteDownFiles() {
 
 addUninstall() {
 	echo "Creating uninstall script..."
-	echo "rm -r ../$game_folder" > uninstall.sh
-	echo "rm ~/Desktop/$launcher_name" >> uninstall.sh
-	echo "rm ~/.local/share/applications/$launcher_name" >> uninstall.sh
+	echo "rm -r ../${game_folder}" > uninstall.sh
+	echo "rm ~/Desktop/${launcher_name}" >> uninstall.sh
+	echo "rm ~/.local/share/applications/${launcher_name}" >> uninstall.sh
 	chmod +x uninstall.sh
 	mv uninstall.sh ./$game_folder
 	echo -e "\xE2\x9C\x94 Uninstall script created"
@@ -255,4 +255,4 @@ addLinks
 deleteDownFiles
 addUninstall
 
-echo -e "\xE2\x9C\x94 Installation completed, execute $game_folder/System$system_suffix/$game_executable$arc_suffix to play"
+echo -e "\xE2\x9C\x94 Installation completed, execute ${game_folder}/System${system_suffix}/${game_executable}${arc_suffix} to play"
