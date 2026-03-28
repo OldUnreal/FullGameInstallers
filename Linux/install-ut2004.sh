c06bf4ed86b3cd09ffbd0d90817ec12eaa145e69
Andrew Moore <me@andrewmoore.ca>
2026-03-28 10:38:03 +1030

    feat: added support for ppc64le for UT2004

--- Linux/install-ut2004.sh
+++ Linux/install-ut2004.sh
@@ -596,6 +596,22 @@ installer::entrypoint() {
       ;;
     esac
     ;;
+  ppc64le)
+    case "${PRODUCT_SHORTNAME}" in
+    UT2004)
+      ARCHITECTURE_SUFFIX='powerpc64le'
+      ARCHITECTURE_BINARY_SUFFIX='-powerpc64le'
+      UE_SYSTEM_FOLDER_SUFFIX='PPC64LE'
+      UEED_SYSTEM_FOLDER_SUFFIX='NOT_SUPPORTED'
+      ;;
+    *)
+      ARCHITECTURE_SUFFIX='NOT_SUPPORTED'
+      ARCHITECTURE_BINARY_SUFFIX=''
+      UE_SYSTEM_FOLDER_SUFFIX=''
+      UEED_SYSTEM_FOLDER_SUFFIX=''
+      ;;
+    esac
+    ;;
   *)
     ARCHITECTURE_SUFFIX='NOT_SUPPORTED'
     ARCHITECTURE_BINARY_SUFFIX=''
@@ -1364,6 +1380,11 @@ installer::entrypoint() {
       return 1
     fi
 
+    if [[ "${_arg_unrealed}" == "on" ]] && [[ "${UEED_SYSTEM_FOLDER_SUFFIX}" == 'NOT_SUPPORTED' ]]; then
+      term::step::failed_with_error "CPU Architecture ${DETECTED_ARCHITECTURE} is not currently supported for UnrealEd. Remove the -e flag."
+      return 1
+    fi
+
     local MISSING_DEPS=()
     local MISSING_DEPS_RHEL=()
     local MISSING_DEPS_DEB=()
@@ -1793,6 +1814,62 @@ You may read the Terms of Service at this URL:
       DOWNLOAD_PATH="${_arg_destination%/}/Installer/${DOWNLOAD_FILENAME}"
 
       local TARGET_STEP_NAME="Download ${DOWNLOAD_FILENAME}"
+
+      # Allow for the .iso and patch files to be alongside the script
+      local POSSIBLE_EXISTING_FILES_TO_CHECK=(
+        "${DOWNLOAD_PATH}"
+        "${_SCRIPT_DIR}/${DOWNLOAD_FILENAME}"
+      )
+
+      term::step::new "${TARGET_STEP_NAME}"
+
+      for POSSIBLE_EXISTING_FILE in "${POSSIBLE_EXISTING_FILES_TO_CHECK[@]}"; do
+        # If the file exist and isn't a partial download
+        if [[ -f "${POSSIBLE_EXISTING_FILE}" ]] && [[ ! -f "${POSSIBLE_EXISTING_FILE}.aria2" ]]; then
+          local EXISTING_FILESIZE EXISTING_HASH
+          EXISTING_FILESIZE=$(stat --format=%s "${POSSIBLE_EXISTING_FILE}")
+          EXISTING_HASH=$(helper::progress::run_with_progress "Verifying existing file" downloader::compute_sha256sum "${POSSIBLE_EXISTING_FILE}")
+
+          local REMAINING_DOWNLOAD_URL_SETS="${DOWNLOAD_URL_SETS}"
+          while true; do
+            # We looped through all the possible values, we can break out
+            if [[ -z "${REMAINING_DOWNLOAD_URL_SETS}" ]]; then
+              break
+            fi
+
+            CURRENT_DOWNLOAD_URL_SET=$(helper::string::unshift::next_value "${REMAINING_DOWNLOAD_URL_SETS}" ";;")
+            REMAINING_DOWNLOAD_URL_SETS=$(helper::string::unshift::remainder "${REMAINING_DOWNLOAD_URL_SETS}" ";;")
+
+            local DOWNLOAD_URL DOWNLOAD_SIZE DOWNLOAD_HASH
+
+            DOWNLOAD_URL=$(helper::string::unshift::next_value "${CURRENT_DOWNLOAD_URL_SET}")
+            CURRENT_DOWNLOAD_URL_SET=$(helper::string::unshift::remainder "${CURRENT_DOWNLOAD_URL_SET}")
+
+            DOWNLOAD_SIZE=$(helper::string::unshift::next_value "${CURRENT_DOWNLOAD_URL_SET}")
+            CURRENT_DOWNLOAD_URL_SET=$(helper::string::unshift::remainder "${CURRENT_DOWNLOAD_URL_SET}")
+
+            DOWNLOAD_HASH=$(helper::string::unshift::next_value "${CURRENT_DOWNLOAD_URL_SET}")
+
+            if [[ -n "${DOWNLOAD_SIZE}" ]] && [[ "${EXISTING_FILESIZE}" -ne "${DOWNLOAD_SIZE}" ]]; then
+              continue
+            fi
+
+            if [[ -n "${DOWNLOAD_HASH}" ]] && [[ "${EXISTING_HASH}" != "${DOWNLOAD_HASH}" ]]; then
+              continue
+            fi
+
+            IS_DOWNLOAD_SKIPPED="yes"
+            break
+          done
+
+          if [[ "${IS_DOWNLOAD_SKIPPED}" == "yes" ]]; then
+            if [[ "${POSSIBLE_EXISTING_FILE}" != "${DOWNLOAD_PATH}" ]]; then
+              ln -s "${POSSIBLE_EXISTING_FILE}" "${DOWNLOAD_PATH}"
+            fi
+            break
+          fi
+        fi
+      done
 
       # If the file exist and isn't a partial download
       if [[ -f "${DOWNLOAD_PATH}" ]] && [[ ! -f "${DOWNLOAD_PATH}.aria2" ]]; then
--- Linux/src/steps/download_files.sh
+++ Linux/src/steps/download_files.sh
@@ -12,6 +12,62 @@ step::download_files() {
 
     local TARGET_STEP_NAME="Download ${DOWNLOAD_FILENAME}"
 
+    # Allow for the .iso and patch files to be alongside the script
+    local POSSIBLE_EXISTING_FILES_TO_CHECK=(
+      "${DOWNLOAD_PATH}"
+      "${_SCRIPT_DIR}/${DOWNLOAD_FILENAME}"
+    )
+
+    term::step::new "${TARGET_STEP_NAME}"
+
+    for POSSIBLE_EXISTING_FILE in "${POSSIBLE_EXISTING_FILES_TO_CHECK[@]}"; do
+      # If the file exist and isn't a partial download
+      if [[ -f "${POSSIBLE_EXISTING_FILE}" ]] && [[ ! -f "${POSSIBLE_EXISTING_FILE}.aria2" ]]; then
+        local EXISTING_FILESIZE EXISTING_HASH
+        EXISTING_FILESIZE=$(stat --format=%s "${POSSIBLE_EXISTING_FILE}")
+        EXISTING_HASH=$(helper::progress::run_with_progress "Verifying existing file" downloader::compute_sha256sum "${POSSIBLE_EXISTING_FILE}")
+
+        local REMAINING_DOWNLOAD_URL_SETS="${DOWNLOAD_URL_SETS}"
+        while true; do
+          # We looped through all the possible values, we can break out
+          if [[ -z "${REMAINING_DOWNLOAD_URL_SETS}" ]]; then
+            break
+          fi
+
+          CURRENT_DOWNLOAD_URL_SET=$(helper::string::unshift::next_value "${REMAINING_DOWNLOAD_URL_SETS}" ";;")
+          REMAINING_DOWNLOAD_URL_SETS=$(helper::string::unshift::remainder "${REMAINING_DOWNLOAD_URL_SETS}" ";;")
+
+          local DOWNLOAD_URL DOWNLOAD_SIZE DOWNLOAD_HASH
+
+          DOWNLOAD_URL=$(helper::string::unshift::next_value "${CURRENT_DOWNLOAD_URL_SET}")
+          CURRENT_DOWNLOAD_URL_SET=$(helper::string::unshift::remainder "${CURRENT_DOWNLOAD_URL_SET}")
+
+          DOWNLOAD_SIZE=$(helper::string::unshift::next_value "${CURRENT_DOWNLOAD_URL_SET}")
+          CURRENT_DOWNLOAD_URL_SET=$(helper::string::unshift::remainder "${CURRENT_DOWNLOAD_URL_SET}")
+
+          DOWNLOAD_HASH=$(helper::string::unshift::next_value "${CURRENT_DOWNLOAD_URL_SET}")
+
+          if [[ -n "${DOWNLOAD_SIZE}" ]] && [[ "${EXISTING_FILESIZE}" -ne "${DOWNLOAD_SIZE}" ]]; then
+            continue
+          fi
+
+          if [[ -n "${DOWNLOAD_HASH}" ]] && [[ "${EXISTING_HASH}" != "${DOWNLOAD_HASH}" ]]; then
+            continue
+          fi
+
+          IS_DOWNLOAD_SKIPPED="yes"
+          break
+        done
+
+        if [[ "${IS_DOWNLOAD_SKIPPED}" == "yes" ]]; then
+          if [[ "${POSSIBLE_EXISTING_FILE}" != "${DOWNLOAD_PATH}" ]]; then
+            ln -s "${POSSIBLE_EXISTING_FILE}" "${DOWNLOAD_PATH}"
+          fi
+          break
+        fi
+      fi
+    done
+
     # If the file exist and isn't a partial download
     if [[ -f "${DOWNLOAD_PATH}" ]] && [[ ! -f "${DOWNLOAD_PATH}.aria2" ]]; then
       term::step::new "${TARGET_STEP_NAME}"
